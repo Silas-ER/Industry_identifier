7304c54ae603a7626201ff5534647f9c903bb677
Silas Eduardo <silasn1@live.com>
2025-03-24 09:13:36 -0300

    feat: criação do app de recepção e adequação da url no projeto

--- fishing_identifier/urls.py
+++ fishing_identifier/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import render # new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('recepcao.urls')), # new
 ]
