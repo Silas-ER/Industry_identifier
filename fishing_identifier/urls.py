90f02e1bd445990b3f9f3888608044c771b8376a
Silas Eduardo <silasn1@live.com>
2025-03-25 11:55:03 -0300

    feat: primeiros passos para implementação do login

--- fishing_identifier/urls.py
+++ fishing_identifier/urls.py
@@ -1,17 +1,35 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import render # new
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 """
     Views Gerais do Projeto
 """
-def login_view(request):
+def login_page(request):
     return render(request, 'generic/login.html')
 
-def home_view(request):
-    return render(request, 'generic/home.html')
+def home(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+    
+        #Autenticação
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login efetuado com sucesso.')
+            redirect('home')
+        else:
+            messages.error(request, 'Usuário ou senha inválidos.')
+            return redirect('login')    
+        
+    else:
+        return render(request, 'generic/login.html')
 
-def about_view(request):
+def about(request):
     return render(request, 'generic/about.html')
 
 """
@@ -19,9 +37,10 @@ def about_view(request):
 """
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', login_view, name='login'), 
-    path('home/', home_view, name='home'),
-    path('about/', about_view, name='about'),
+    path('', login_page, name='login'), 
+    path('home/', home, name='home'),
+    path('about/', about, name='about'),
+    path('logout/', logout, name='logout'),
     
     # Urls de Apps
     path('recepcao/', include('recepcao.urls')),
